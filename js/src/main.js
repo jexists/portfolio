460e94d7e187d1b92144483082d74cce608b535e
jexists <jexists486@gmail.com>
2019-12-11 12:52:48 +0900

    basic files on 191211

---
+++ js/src/main.js
@@ -0,0 +1,17 @@
+// main.js
+
+(function($){
+	const body = $('body');
+	const wrap = $('#wrap');
+	wrap.append('<section id="viewBox"></section>');
+	const viewBox = $('#viewBox');
+	//script 내용 간편작성함수 생성
+	const JsLink = (temp) => {
+		return body.append(`<script src="../js/src/temp/${temp}.js"></script>`);
+	};
+	viewBox.load('./temp/slide_01.html', () => {
+		JsLink('slide_01');
+	});
+
+
+})(jQuery);
