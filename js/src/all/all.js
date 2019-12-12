f832e6ed9db5b7bb09ae80b2208813e842c3875d
jexists <jexists486@gmail.com>
2019-12-12 09:22:52 +0900

    home on 191211

---
+++ js/src/all/all.js
@@ -0,0 +1,19 @@
+
+
+(function(){
+ const search = $('.search');
+ const searchLs = $('.searchlist');
+ const searchLogo = $('.searchlogo');
+
+ search.on('mouseenter',function(){
+ 	$(this).addClass('action');
+ 	searchLogo.addClass('action');
+ 	searchLs.stop().slideDown(500);
+ });
+ search.on('mouseleave',function(){
+ 	$(this).removeClass('action');
+ 	searchLogo.removeClass('action');
+ 	searchLs.stop().slideUp(300);
+ })
+
+})(jQuery);
