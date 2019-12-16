3326825aa1226efa6536cf984ce2c55a9a078429
jexists <jexists486@gmail.com>
2019-12-16 09:21:05 +0900

    responsive on 191215

--- js/src/all/all.js
+++ js/src/all/all.js
@@ -29,4 +29,15 @@
 
 	});
 	
+  
+ const conbox = $('.conbox');
+
+ conbox.find('li').on('mouseenter',function(){
+ 	$(this).find('img').addClass('action');
+ });
+ conbox.find('li').on('mouseleave',function(){
+ 	$(this).find('img').removeClass('action');
+ });
+
+
 })(jQuery);
